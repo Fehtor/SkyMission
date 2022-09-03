b6f0369fd4a7edc4c27656cf3f6abdfe32229e31
Fehtor <f.truhachiov@mail.ru>
2022-09-03 11:38:23 +0300

    InitialCommit

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    float horizontal;
+    float vertical;
+    Rigidbody2D rb;
+    public float speed;
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        rb.velocity = new Vector2(horizontal, vertical) * speed;
+
+        if(horizontal != 0)
+        {
+            anim.SetBool("isRunning", true);
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
+        }
+    }
+}
